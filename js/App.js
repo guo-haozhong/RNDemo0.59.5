22cccd381d9be08c57a6711318f08ae738e3274c
fendandrain <1033682873@qq.com>
2019-05-22 13:41:42 +0800

    change file path

--- js/App.js
+++ js/App.js
@@ -9,26 +9,17 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+ class Root extends Component{
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
 }
-
+export default Root
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- js/index.js
+++ js/index.js
@@ -2,7 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-
+if (!__DEV__) {
+    console = {
+        log: () => { },
+        error: () => { },
+        info: () => { },
+        warn: () => { },
+    }
+}
 AppRegistry.registerComponent('tempnew', () => App);
